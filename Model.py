2dba36ea227658fd73f77ed4586370ad2aaf9e05
Sina Haji Kazemi <sina.hkazemi@gmail.com>
2022-12-20 11:33:39 +0100

    model file revised completely

--- Model.py
+++ Model.py
@@ -1,224 +1,129 @@
-# file  -- Module.py --
+import numpy as np
 import pyomo.environ as pyo
-from pyomo.opt import SolverFactory
+from pyomo.opt import SolverFactory, SolverStatus, TerminationCondition
 
+
+# configure the parameters of the original pv house model
 def getSettings():
-    """Return settingsDict with default settings for usage in an HouseModel class object"""
     settingsDict = {
-                    "lifetime": 10,  # Years
-                    "cost_PV": 1000,  # €/kW
-                    "cost_Battery": 300,  # €/kWh
-                    "cost_buy": 0.25,  # €/kWh
-                    "dem_tot": 3500,  # kWh/Year
-                    }
+        "Lifetime": 10,  # Years
+        "Cost_PV": 1000,  # €/kW
+        "Cost_Battery": 300,  # €/kWh
+        "Cost_buy": 0.25,  # €/kWh
+        "Sell_price": 0.05,  # €/kWh
+        "Demand_total": 3500,  # kWh/Year
+    }
     return settingsDict
 
 
-class HouseModel:
-    """Create an instance of the House model, setting its basic parameters"""
-
-    def __init__(self, *settings_dict):
-        if not settings_dict:
-            self.Settings = getSettings()
-        else:
-            self.Settings = settings_dict[0]
-
-    def sample_model(self, input_changes=None, fixed_outputs=None):
+# create HouseModel
+class HouseModel():
+    def __init__(self, settings: dict[str, int|float], PV_availability: list[float], Demand: list[float]):
         # Step 0: Create an instance of the model
         model = pyo.ConcreteModel()
+        self.model = model
 
-        # Step 1: Define index sets
-        time = range(8760)
+        # Step 1.1: Define index sets
+        time = range(8760) # hours in one year
 
-        # Input Changes
-        Names = ["CostPV", "CostBat", "CostBuy", "Demand"]
-        Scaling = [1, 1, 1, 1, 1, 1, 1, 1, 1]
-        if input_changes is not None:
-            for i in range(Names.__len__()):
-                try:
-                    Scaling[i] = input_changes[Names[i]]
-                except:
-                    Scaling[i] = 1
+        # Step 1.2: Parameters
+        Lifetime = settings["lifetime"]  # lifetime in years
+        Cost_PV = settings["cost_PV"] / Lifetime  # € / (lifetime * kW)
+        Cost_battery = settings["Cost_Battery"] / Lifetime  # € / (lifetime * kWh)
+        Cost_buy = settings["Cost_buy"]  # € / kWh
+        Demand_total = settings["Demand_total"]  # kWh
+        Sell_price = settings["Sell_price"]  # € / kWh
 
-        # Output Fixes
-        Names = ["PVFixed", "BatteryFixed", "SelfProdRatioFixed", "TOTEXFixed", "CAPEXFixed"]
-        Fixing = [-1, -1, -1, -1, -1, -1, -1, -1]
-        if fixed_outputs is not None:
-            for i in range(Names.__len__()):
-                try:
-                    Fixing[i] = fixed_outputs[Names[i]]
-                except:
-                    Fixing[i] = -1
+        # Step 2: Define the decision variables
+        # Electricity sector
+        model.energy_PV = pyo.Var(time, within=pyo.NonNegativeReals)
+        model.energy_battery = pyo.Var(time, within=pyo.NonNegativeReals)
+        model.energy_battery_IN = pyo.Var(time, within=pyo.NonNegativeReals)
+        model.energy_battery_OUT = pyo.Var(time, within=pyo.NonNegativeReals)
+        model.energy_buy = pyo.Var(time, within=pyo.NonNegativeReals)
+        model.capacity_PV = pyo.Var(within=pyo.NonNegativeReals)
+        model.capacity_battery = pyo.Var(within=pyo.NonNegativeReals)
+        model.sell_energy = pyo.Var(time, within=pyo.NonNegativeReals)
 
-        # Step 1.5: Parameters
-        lifetime = self.Settings["lifetime"]  # years
-        cost_PV = Scaling[0] * self.Settings["cost_PV"] / lifetime  # *((time[-1]+1)/8760) # € / (lifetime * kW)
-        cost_Battery = Scaling[1] * self.Settings[
-            "cost_Battery"] / lifetime  # *((time[-1]+1)/8760)  # € / (lifetime * kWh)
-        cost_buy_ele = Scaling[2] * self.Settings["cost_buy"]  # €/kWh
-        dem_tot = Scaling[3] * self.Settings["dem_tot"]  # kWh
-        battery_in_eff = 1  # efficiency 100%
-
-        import csv
-
-        availability_pv = []  # create empty arrays
-        DemandVal = []
-
-        with open('TS_PVAvail.csv', 'r') as file:
-            # next(file)
-            reader = csv.reader(file, delimiter='\n')
-            for row in reader:
-                availability_pv.append(float(row[0]))
-
-        with open('TS_Demand.csv', 'r') as file:
-            # next(file)
-            reader = csv.reader(file, delimiter='\n')
-            for row in reader:
-                DemandVal.append(float(row[0]))
-
-        availability_pv = dict(enumerate(availability_pv))
-        DemandVal = dict(enumerate(DemandVal))
-
-
-        # Step 2: Define the decision
-
-        # Electricity Sector
-        model.EnergyPV = pyo.Var(time, within=pyo.NonNegativeReals)
-        model.Demand = pyo.Var(time, within=pyo.NonNegativeReals)
-        model.EnergyBattery = pyo.Var(time, within=pyo.NonNegativeReals)
-        model.EnergyBattery_IN = pyo.Var(time, within=pyo.NonNegativeReals)
-        model.EnergyBattery_OUT = pyo.Var(time, within=pyo.NonNegativeReals)
-        model.EnergyBuy = pyo.Var(time, within=pyo.NonNegativeReals)
-        model.CapacityPV = pyo.Var(within=pyo.NonNegativeReals)
-        model.CapacityBattery = pyo.Var(within=pyo.NonNegativeReals)
-        model.CostBuy = pyo.Var(within=pyo.Reals)
-        model.CostPV = pyo.Var(within=pyo.Reals)
-        model.CostBat = pyo.Var(within=pyo.Reals)
-
-        # Step 3: Define Objective
-        model.cost = pyo.Objective(expr=cost_PV * model.CapacityPV + cost_buy_ele * sum(
-            model.EnergyBuy[i] for i in time) + cost_Battery * model.CapacityBattery, sense=pyo.minimize)
+        # Step 3: Define objective
+        model.cost = pyo.Objective(
+            expr=Cost_PV * model.capacity_PV
+            + Cost_buy * sum(model.energy_buy[i] for i in time)
+            + Cost_battery * model.capacity_battery
+            - Sell_price * sum(model.sell_energy[i] for i in time),
+            sense=pyo.minimize,
+        )
 
         # Step 4: Constraints
-        model.limEQ = pyo.ConstraintList()
-
+        model.limEQpv = pyo.ConstraintList()
         for i in time:
-            model.limEQ.add(model.EnergyPV[i] <= model.CapacityPV * availability_pv[i]) # PV Upper Limit
+            model.lim_eq_PV.add(model.energy_PV[i] <= model.capacity_PV * PV_availability[i])  # PV Upper Limit
 
+        model.limEQbat = pyo.ConstraintList()
         for i in time:
-            model.limEQ.add(model.EnergyBattery[i] <= model.CapacityBattery)  # Battery Upper Limit
-
-        model.InitialBattery = pyo.Constraint(
-            expr=model.EnergyBattery[0] == model.EnergyBattery[time[-1]] - model.EnergyBattery_OUT[0] +
-                 model.EnergyBattery_IN[0])  # Battery level t=0 == t=T
-
-        model.DemandEQ = pyo.ConstraintList()
-
-        for i in time:
-            model.DemandEQ.add(expr=model.Demand[i] == dem_tot * DemandVal[i])  # Electricity Demand
+            model.limEQbat.add(model.energy_battery[i] <= model.capacity_battery)  # Battery Upper Limit
 
         model.batteryEQ = pyo.ConstraintList()
-
-        for i in time[1:]:
+        for i in time:
             model.batteryEQ.add(
-                expr=model.EnergyBattery[i] == model.EnergyBattery[i - 1] - model.EnergyBattery_OUT[i] +
-                     model.EnergyBattery_IN[i])  # Battery Equation
+                expr=model.energy_battery[i]
+                == model.energy_battery[time[i - 1]] - model.energy_battery_out[i] + model.energy_battery_in[i]
+            )  # Battery Equation
 
         model.EnergyEQ = pyo.ConstraintList()
         for i in time:
             model.EnergyEQ.add(
-                expr=model.Demand[i] == model.EnergyBuy[i] + model.EnergyBattery_OUT[i] - model.EnergyBattery_IN[
-                    i] + model.EnergyPV[i])  # Energy Equation
+                expr=Demand_total  * Demand[i]
+                == model.energy_buy[i]
+                + model.energy_battery_out[i]
+                - model.energy_battery_in[i]
+                + model.energy_PV[i]
+                - model.sell_energy[i]
+            )  # Energy Equation
 
-        # Some equations that store input settings in a Variable
-        model.ValueCostBuy = pyo.Constraint(expr=model.CostBuy == cost_buy_ele)
-        model.ValueCostPV = pyo.Constraint(expr=model.CostPV == cost_PV)
-        model.ValueCostBat = pyo.Constraint(expr=model.CostBat == cost_Battery)
-
-        # Equations to fix outputs
-        if Fixing[0] != -1:  # fixed PV Cap
-            model.fixedPV = pyo.Constraint(expr=model.CapacityPV == Fixing[0])
-
-        if Fixing[1] != -1:  # fixed Battery cap
-            model.fixedBattery = pyo.Constraint(expr=model.CapacityBattery == Fixing[1])
-
-        if Fixing[2] != -1:  # fixed Self Generation
-            model.SelfProduction = pyo.Constraint(expr=sum(model.EnergyPV[i] for i in time) / dem_tot == Fixing[2])
-
-        if Fixing[3] != -1:  # fixed TOTEX
-            model.TOTEX = pyo.Constraint(expr=cost_PV * model.CapacityPV + cost_buy_ele * sum(
-                model.EnergyBuy[i] for i in time) + cost_Battery * model.CapacityBattery == Fixing[3])
-
-        if Fixing[4] != -1:  # fixed CAPEX
-            model.CAPEX = pyo.Constraint(
-                expr=cost_PV * model.CapacityPV + cost_Battery * model.CapacityBattery == Fixing[4])
-
-        # Change lines below to use other solver
-        solver_options = open("solverSettings.txt", "r").read().split("\n")
-        for i in solver_options:
-            if i.find('#') == 0 or i.__len__() == 0:
-                val = 0  # comment or empty line: do nothing
-            elif i.find('solver') == 0:
-                val = i.split('=')
-                val = val[1].strip()
-                # set solver
-                solver = SolverFactory(val)
-            else:
-                val = i.split('=')
-                opt = val[0].strip()
-                val = val[1].strip()
-                try:
-                    val2 = float(val)
-                except ValueError:
-                    val2 = val
-
-                solver.options[opt] = val2
-
-        results = solver.solve(model, tee=True, keepfiles=True)
-        results.write()
-        # Print full model to console. Only enable for debugging purpose
-        # model.pprint()
-        return model, results.solver.termination_condition
-
-
-def getKPI(model, *base_model):
-    from pyomo.opt import TerminationCondition as TC
-    if model[1] == TC.infeasible:
-        KPIdict = {"Cap_PV": -2, "Cap_Bat": -2, "Own_Gen": -2, "TOTEX": -2, "CAPEX": -2}
-    else:
-        # read the values from a solved model
-        model = model[0]
-        import pyomo.environ as pyo
-
-        # get some key information from the model
-        time_steps = range(model.Demand.__len__())
-        demand_tot = sum(pyo.value(model.Demand[i]) for i in time_steps)
-        cost_buy_ele = pyo.value(model.CostBuy)
-        cost_PV = pyo.value(model.CostPV)
-        cost_Battery = pyo.value(model.CostBat)
-
-        Value_PV = pyo.value(model.CapacityPV)
-        Value_Bat = pyo.value(model.CapacityBattery)
-        Value_OwnGen = sum(pyo.value(model.EnergyPV[i]) for i in time_steps) / demand_tot
-        Value_TOTEX = cost_buy_ele * sum(pyo.value(model.EnergyBuy[i]) for i in time_steps) + cost_PV * pyo.value(
-            model.CapacityPV) + cost_Battery * pyo.value(model.CapacityBattery)
-        Value_CAPEX = cost_PV * pyo.value(model.CapacityPV) + cost_Battery * pyo.value(model.CapacityBattery)
-
-        if not base_model:
-            KPIdict = {
-                "Cap_PV": Value_PV,
-                "Cap_Bat": Value_Bat,
-                "Own_Gen": Value_OwnGen,
-                "TOTEX": Value_TOTEX,
-                "CAPEX": Value_CAPEX
-            }
+        # Write model to mps file
+        # model.write(filename=r"model.mps", io_options={"symbolic_solver_labels": True})
+    def solve(self, solver_name = "cplex"):
+        solver = SolverFactory(solver_name)
+        solver_output = solver.solve(self.model)
+        if (solver_output.solver.status == SolverStatus.ok) and (solver_output.solver.termination_condition == TerminationCondition.optimal):
+            # Do something when the solution in optimal and feasible
+            print("Termination Condition is Optimal")
+        elif (solver_output.solver.termination_condition == TerminationCondition.infeasible):
+            # Do something when model in infeasible
+            print("Termination Condition is Infeasible")
         else:
-            base_model = base_model[0]
-            KPIdict = {  # % difference to base model
-                "Cap_PV": (Value_PV - base_model["Cap_PV"]) / (base_model["Cap_PV"] + 1e-8),
-                "Cap_Bat": (Value_Bat - base_model["Cap_Bat"]) / (base_model["Cap_Bat"] + 1e-8),
-                "Own_Gen": (Value_OwnGen - base_model["Own_Gen"]) / (base_model["Own_Gen"] + 1e-8),
-                "TOTEX": (Value_TOTEX - base_model["TOTEX"]) / (base_model["TOTEX"] + 1e-8),
-                "CAPEX": (Value_CAPEX - base_model["CAPEX"]) / (base_model["CAPEX"] + 1e-8)
-            }
-    return KPIdict
+            # Something else is wrong
+            print("Solver Status:" ,  solver_output.solver.status)
+
+    def get_output(self):
+        return None
+
+
+# run model
+def run():
+    # start = time.time()
+    settings = getSettings()
+    PV_availability = np.loadtxt("../time_series/original_pv_availability.csv")
+    Demand = np.loadtxt("../time_series/bdew_demand.csv")
+    solution = HouseModel(settings, PV_availability, Demand)
+    # end = time.time()
+
+    # print output
+    print("Capacity for the PV module: " + str(pyo.value(solution.CapacityPV)) + " kW")
+    print("Capacity for the battery module: " + str(pyo.value(solution.CapacityBattery)) + " kWh")
+
+    cost_PV_year = settings["cost_PV"] / settings["lifetime"]
+    cost_BAT_year = settings["cost_Battery"] / settings["lifetime"]
+    print("\nCost of PV module per year: " + str(cost_PV_year) + " €")
+    print("Cost of battery module per year: " + str(cost_BAT_year) + " €")
+    print(
+        "CapEx per year: "
+        + str(cost_PV_year * pyo.value(solution.CapacityPV) + cost_BAT_year * pyo.value(solution.CapacityBattery))
+        + " €"
+    )
+
+    # print("\nExecution of the model took " + str(round(end - start, 2)) + " seconds")
+
+
+run()
